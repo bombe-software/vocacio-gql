b06748fa5200e5efb7f9191cac95d880eeef0e78
Vicroni <drasa_tec@hotmail.com>
2018-10-31 14:37:27 -0600

    Cambios de correcion 2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const createServer = require('http').createServer;
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 //Librerias de la base de datos
 const mongoose = require('mongoose');
