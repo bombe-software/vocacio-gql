7d3c5d5606e52c00a624792ce2abd6bc2964eb55
Vicroni <drasa_tec@hotmail.com>
2018-11-02 10:21:37 -0600

    Cambios en la URI de mongo

--- config/config.js
+++ config/config.js
@@ -18,5 +18,7 @@ exports.api = 'http://localhost:9000';
 exports.ws = "ws://localhost:9000";
 exports.web = "http://localhost:3000";
 exports.status = false;
+//mongodump -h ds147233.mlab.com:47233 -d nullname -u admin -p n0m3l0 -o C:\Users\drasa\Desktop\NullName\api_nullname
+//mongorestore -d nullname C:\Users\drasa\Desktop\NullName\api_nullname\nullname
 exports.MONGO_URI = `mongodb://localhost/nullname`;
 */
